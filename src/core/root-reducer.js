86dfce90716d7d2a2b5997fc3450789bad7f9928
tarikneradin <tarixx@live.com>
2019-12-04 23:54:05 +0100

    Implements app state managamanet

--- src/core/root-reducer.js
+++ src/core/root-reducer.js
@@ -1,6 +1,22 @@
 import { combineReducers } from "redux";
 
-const appReducer = combineReducers({});
+import { flowers } from "../core/features/flowers/reducers/flowers-reducers";
+import { signup } from "../core/features/signup/reducers/signup-reducers";
+import { infoModal } from "../core/features/shared-components/fs-info-modal/info-modal-reducers";
+import { login } from "../core/features/login/reducers/login-reducers";
+import { userProfile } from "../core/features/user-profile/reducers/user-profile-reducers";
+import { mobileNavigation } from "../core/features/mobile-navigation/reducers/mobile-navigation-reducers";
+import { mediaQuery } from "../core/features/media-query/reducers/media-query-reducer";
+
+const appReducer = combineReducers({
+  flowers,
+  signup,
+  infoModal,
+  login,
+  userProfile,
+  mobileNavigation,
+  mediaQuery
+});
 
 export const rootReducer = (state, action) => {
   return appReducer(state, action);
